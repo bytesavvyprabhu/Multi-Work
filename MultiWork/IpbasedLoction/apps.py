c4ecf2f455df60b3ee95c6f4bc3231b8e82961dc
pds <pds240297@gmail.com>
2022-04-08 00:33:19 +0530

    architeture

---
+++ MultiWork/IpbasedLoction/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IpbasedloctionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'IpbasedLoction'
---
+++ MultiWork/base64Image/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Base64ImageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'base64Image'
